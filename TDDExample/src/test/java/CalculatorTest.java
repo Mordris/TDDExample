82417aded997ba2992c543f47db18de3ecb880fb
Yunus Emre Gültepe <118046112+Mordris@users.noreply.github.com>
2023-05-25 17:35:21 +0300

    Add files via upload

---
+++ TDDExample/src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    @Test
+    void testDivision6() {
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5, 0),
+                "IllegalArgumentException expected."
+        );
+
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 5, 2.5",
+            "10, 2.5, 4",
+            "12.5, 2.5, 5"
+    })
+    void testDivision(double dividend, double divisor, double expectedResult) {
+        assertEquals(expectedResult, Calculator.divide(dividend, divisor));
+    }
+}
